590db30d7e2fec1089dbe2e11241dbf574cffacc
PatrikZh <pzheji@actirana.edu.al>
2024-03-07 12:12:33 +0100

    Just pushin from localy to globaly

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function def cat_arrays(arr1, arr2): 
+that concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    code below
+    """
+    concatenated = arr1 + arr2
+    return concatenated
