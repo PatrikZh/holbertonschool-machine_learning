99fafd2e94f5306f25c0693bbf52115dd8361e6d
PatrikZh <pzheji@actirana.edu.al>
2024-03-07 12:25:54 +0100

    Just pushin from localy to globaly

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+function def np_matmul(mat1, mat2):
+that performs matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """code below"""
+    return np.matmul(mat1, mat2)
