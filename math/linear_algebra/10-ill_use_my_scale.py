ece41a192d566f5ffa738e5a9f36c45bb3357e92
PatrikZh <pzheji@actirana.edu.al>
2024-03-07 12:19:24 +0100

    Just pushin from localy to globaly

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+function def np_shape(matrix):
+that calculates the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """code below"""
+    return matrix.shape
