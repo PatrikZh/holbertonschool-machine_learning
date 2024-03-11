5f5afc63699488fb72a34e2be847fa4e46734030
PatrikZh <pzheji@actirana.edu.al>
2024-03-11 15:11:13 +0100

    Annoying when you finish locally but you have to deal with the checker globally

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/ python3
 """
 Normal distribution class evolved with each senquence of code
 """
@@ -25,6 +25,15 @@ class Normal:
             self.mean = sum(data) / len(data)
             self.stddev = (sum((ent - self.mean) ** 2 for ent in data)
                            / len(data)) ** 0.5
+
+     def pdf(self, x):
+        """
+        pdf function
+        """
+        p = 3.1415926536
+        e = 2.7182818285
+        return ((1 / (self.stddev * ((2 * p) ** 0.5))) *
+                (e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2))
 
     def z_score(self, x):
         """
