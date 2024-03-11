8e56aa912edbc1670a77ef881cee3da5819b0110
PatrikZh <pzheji@actirana.edu.al>
2024-03-11 15:32:15 +0100

    Annoying when you finish locally but you have to deal with the checker globally

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -45,10 +45,6 @@ class Normal:
         coeff = 1 / (self.stddev * (2 * pi) ** 0.5)
         exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
         return coeff * (e ** exponent)
-
-    def pdf(self, x):
-        """Assuming implementation is here"""
-        pass
     
     def cdf(self, x):
         """A very simple and inaccurate approximation of CDF for educational purposes."""
