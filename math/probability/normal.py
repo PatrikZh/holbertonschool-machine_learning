ba9d9f886d11020114e8ae8729a628520604fbe5
PatrikZh <pzheji@actirana.edu.al>
2024-03-24 22:48:01 +0100

    Update

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -6,21 +6,23 @@ class Normal:
             if not isinstance(data, list) or len(data) < 2:
                 raise ValueError("data must be a list with multiple values")
             self.mean = sum(data) / len(data)
-            self.stddev = (sum((x - self.mean) ** 2 for x in data) / (len(data))) ** 0.5
+            self.stddev = (sum((x - self.mean) ** 2 for x in data) / len(data)) ** 0.5
         else:
-            if stddev <= 0:
-                raise ValueError("stddev must be a positive value")
             self.mean = mean
             self.stddev = stddev
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
 
     def cdf(self, x):
-        """Calculate the CDF for a given x using a simple approximation."""
+        """Approximate the CDF for a given x using a simple polynomial approximation."""
         z = (x - self.mean) / self.stddev
-        t = 1 / (1 + 0.2316419 * abs(z))
-        cdf_approx = 1 - 1/(2.5066282746310002) * (t * (0.319381530 + t * (-0.356563782 + t * (1.781477937 + t * (-1.821255978 + t * 1.330274429))))) * (2.718281828459045 ** (-z**2 / 2))
+        t = 1.0 / (1.0 + 0.2316419 * z)
+        coeffs = [0.319381530, -0.356563782, 1.781477937, -1.821255978, 1.330274429]
+        cdf_approx = 1 - (1 / (2 * 3.141592653589793) ** 0.5) * \
+                     (t * (coeffs[0] + t * (coeffs[1] + t * (coeffs[2] + t * (coeffs[3] + t * coeffs[4]))))) * \
+                     (2.718281828459045 ** (-z ** 2 / 2))
         if z < 0:
             return 1 - cdf_approx
         return cdf_approx
-
 
 
