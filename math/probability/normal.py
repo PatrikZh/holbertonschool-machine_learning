b7075d590289f745a84bfea4fbf43fbd73007212
PatrikZh <pzheji@actirana.edu.al>
2024-03-24 22:36:41 +0100

    Update

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import math
-
 class Normal:
     """
     the normal class initiating
@@ -21,33 +19,45 @@ class Normal:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.mean = sum(data) / len(data)
-            self.stddev = (sum((ent - self.mean) ** 2 for ent in data)
-                           / len(data)) ** 0.5
+            self.stddev = (sum((ent - self.mean) ** 2 for ent in data) / len(data)) ** 0.5
 
     def z_score(self, x):
         """
         z score function
         """
-        return float((x - self.mean) / self.stddev)
+        return (x - self.mean) / self.stddev
 
     def x_value(self, z):
         """
         x value function
         """
-        return float(self.mean + z * self.stddev)
+        return self.mean + z * self.stddev
 
     def pdf(self, x):
         """
         Approximate the probability density function for a given x.
         """
-        pi = math.pi
-        e = math.e
+        pi = 3.141592653589793
+        e = 2.718281828459045
         coeff = 1 / (self.stddev * (2 * pi) ** 0.5)
         exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
         return coeff * (e ** exponent)
     
     def cdf(self, x):
         """
-        Calculate the cumulative distribution function for a given x using the error function.
+        A very simple and inaccurate approximation of CDF for educational purposes.
         """
-        return 0.5 * (1 + math.erf((x - self.mean) / (self.stddev * math.sqrt(2))))
+        z = (x - self.mean) / self.stddev
+        return 0.5 * (1 + self.sign(z) * (1 - 1 / (1 + abs(z))))
+
+    def sign(self, x):
+        """
+        Utility function to return the sign of x.
+        """
+        if x > 0:
+            return 1
+        elif x < 0:
+            return -1
+        else:
+            return 0
+
