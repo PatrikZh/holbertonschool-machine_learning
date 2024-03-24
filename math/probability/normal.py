44c64993a0b0273664cc5720429118f18f7f5623
PatrikZh <pzheji@actirana.edu.al>
2024-03-24 22:52:15 +0100

    Update

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -15,14 +15,14 @@ class Normal:
 
     def cdf(self, x):
         """Approximate the CDF for a given x using a simple polynomial approximation."""
-        z = (x - self.mean) / self.stddev
-        t = 1.0 / (1.0 + 0.2316419 * z)
-        coeffs = [0.319381530, -0.356563782, 1.781477937, -1.821255978, 1.330274429]
-        cdf_approx = 1 - (1 / (2 * 3.141592653589793) ** 0.5) * \
-                     (t * (coeffs[0] + t * (coeffs[1] + t * (coeffs[2] + t * (coeffs[3] + t * coeffs[4]))))) * \
-                     (2.718281828459045 ** (-z ** 2 / 2))
-        if z < 0:
-            return 1 - cdf_approx
-        return cdf_approx
+        mean = self.mean
+        stddev = self.stddev
+        pi = 3.1415926536
+        value = (x - mean) / (stddev * (2 ** (1 / 2)))
+        erf = value - ((value ** 3) / 3) + ((value ** 5) / 10)
+        erf = erf - ((value ** 7) / 42) + ((value ** 9) / 216)
+        erf *= (2 / (pi ** (1 / 2)))
+        cdf = (1 / 2) * (1 + erf)
+        return cdf
 
 
