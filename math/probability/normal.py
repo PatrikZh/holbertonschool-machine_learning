0fcc2caf40b975f2d1fbe4846bcc4b13581bf231
PatrikZh <pzheji@actirana.edu.al>
2024-03-24 22:59:59 +0100

    Update

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -5,12 +5,16 @@ Normal file
 
 
 class Normal:
+    """
+    the exponential class
+    """
     def __init__(self, data=None, mean=0, stddev=1.):
         if data is not None:
             if not isinstance(data, list) or len(data) < 2:
                 raise ValueError("data must be a list with multiple values")
             self.mean = sum(data) / len(data)
-            self.stddev = (sum((x - self.mean) ** 2 for x in data) / len(data)) ** 0.5
+            self.stddev = (sum((x - self.mean) ** 2 for x in data) 
+            / len(data)) ** 0.5
         else:
             self.mean = mean
             self.stddev = stddev
@@ -18,7 +22,10 @@ class Normal:
                 raise ValueError("stddev must be a positive value")
 
     def cdf(self, x):
-        """Approximate the CDF for a given x using a simple polynomial approximation."""
+        """
+        Approximate the CDF for a given x 
+        using a simple polynomial approximation.
+        """
         mean = self.mean
         stddev = self.stddev
         pi = 3.1415926536
