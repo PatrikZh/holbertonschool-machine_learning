5e10c0464f9fa9c45f039b99f2513c8b9dbe5a16
PatrikZh <pzheji@actirana.edu.al>
2024-03-25 15:40:22 +0100

    First commit

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A class Neuron that defines a single neuron performing binary classification
+"""
+
+
+import numpy as py
+
+
+class Neuron:
+    """
+    A single neuron performing binary classification
+    """
+    pass
