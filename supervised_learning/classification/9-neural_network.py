38e880704b56bd266716aefd9aaeb6f029c27c3c
PatrikZh <pzheji@actirana.edu.al>
2024-03-28 16:48:37 +0100

    Doing exercise 10 on classification

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -30,23 +30,23 @@ class NeuralNetwork:
         self.__b2 = 0
         self.__A2 = 0
 
-        @property
-        def W1(self):
+    @property
+    def W1(self):
             """ get method for property W1"""
             return self.__W1
 
-        @property
-        def b1(self):
+    @property
+    def b1(self):
             """ get method for property b1"""
             return self.__b1
 
-        @property
-        def A1(self):
+    @property
+    def A1(self):
             """ get method for property A1"""
             return self.__A1
 
-        @property
-        def W2(self):
+    @property
+    def W2(self):
             """ get method for property W2"""
             return self.__W2
 
@@ -55,7 +55,7 @@ class NeuralNetwork:
             """ get method for property b2"""
             return self.__b2
 
-        @property
-        def A2(self):
+    @property
+    def A2(self):
             """ get method for property A2"""
             return self.__A2
