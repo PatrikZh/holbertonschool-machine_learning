4d2b4a3cdee4afe89e497fccc995605a344aed1c
PatrikZh <pzheji@actirana.edu.al>
2024-03-28 16:26:11 +0100

    Doing exercise 8 on classification

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+A class Neuron that defines a single neuron performing binary classification
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """
+    A single neuron performing binary classification
+    """
+    def __init__(self, nx, nodes):
+        """class constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nx must be an integer")
+        if nodes < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
