36e6016a879daabcae8f84ab860ecdf7836ebe17
PatrikZh <pzheji@actirana.edu.al>
2024-03-28 16:30:09 +0100

    Doing exercise 8 on classification

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -18,9 +18,9 @@ class NeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(nodes, int):
-            raise TypeError("nx must be an integer")
+            raise TypeError("nodes must be an integer")
         if nodes < 1:
-            raise ValueError("nx must be a positive integer")
+            raise ValueError("nodes must be a positive integer")
 
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
