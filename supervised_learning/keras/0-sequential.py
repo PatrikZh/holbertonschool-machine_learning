b9338a155ffd2b88e92efcce8d3bcdcd1d69faad
PatrikZh <pzheji@actirana.edu.al>
2024-04-08 16:01:14 +0200

    Hello Tenserflow

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Write a function def build_model(nx, layers, activations, lambtha, keep_prob)
+that builds a neural network with the Keras library
+"""
+
+import tenserflow.keras as k
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """ 
+nx is the number of input features to the network
+layers is a list containing the number of nodes in each layer of the network
+activations is a list containing the activation functions 
+used for each layer of the network
+lambtha is the L2 regularization parameter
+keep_prob is the probability that a node will be kept for dropout
+    """
+    model = K.Sequential()
+
+    for i in range(len(layers)):
+        model.add(K.layers.Dense(layers[i],
+                  activation=activations[i],
+                  kernel_regularizer=K.regularizers.L2(lambtha),
+                  input_dim=nx))
+        # apply dropout except on output layer
+        if i != len(layers) - 1 and keep_prob is not None:
+            model.add(K.layers.Dropout(1-keep_prob))
+
+    return model
